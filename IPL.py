2368b1daadbd1726d70ef28720ebd2009e864bb1
mukesh kumar mahto <mukesh.7755@gmail.com>
2022-12-16 12:43:07 +0530

    change

--- IPL.py
+++ IPL.py
@@ -3,5 +3,5 @@ Name=str(input("Enter Batsman Name :: "))
 Runs=int(input("Enter run :: "))
 Ball=int(input(" Enter Ball:: "))
 strikerate=((Runs/Ball)*100)
-print("%s Has played %d Ball and Runs score %d and his strike rate is %d "%(Name,Runs,Ball,strikerate))
+print("%s Has played %d Ball and Runs score %d and  strike rate is %d "%(Name,Runs,Ball,strikerate))
 
